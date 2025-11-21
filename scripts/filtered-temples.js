eed5b17828269cd32f080a5fde5bdc410de56d86
goodluck-aberare <aberaregoodluck@yahoo.com>
2025-11-21 12:27:44 +0100

    v2

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -21,9 +21,8 @@ const temples = [
     { templeName: "Washington D.C.", location: "Kensington, Maryland, United States", dedicated: "1974, November, 19", area: 156558, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg" },
     { templeName: "Lima Perú", location: "Lima, Perú", dedicated: "1986, January, 10", area: 9600, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg" },
     { templeName: "Mexico City Mexico", location: "Mexico City, Mexico", dedicated: "1983, December, 2", area: 116642, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg" },
-    { templeName: "Accra Ghana", location: "Accra, Ghana", dedicated: "2004, January, 11", area: 17500, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/400x250/accra-ghana-temple-lds-273999-wallpaper.jpg" },
-    { templeName: "Rome Italy", location: "Rome, Italy", dedicated: "2019, March, 10", area: 41010, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/rome-italy/400x250/rome-italy-mormon-temple-2195528.jpg" },
-    { templeName: "Salt Lake Utah", location: "Salt Lake City, Utah, United States", dedicated: "1893, April, 6", area: 382207, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/salt-lake-temple/400x250/salt-lake-temple-lds-281251-wallpaper.jpg" }
+    { templeName: "Accra Ghana", location: "Accra, Ghana", dedicated: "2004, January, 11", area: 17500, imageUrl: "https://churchofjesuschristtemples.org/accra-ghana-temple/photographs/#Gallery-13" },
+    { templeName: "Salt Lake Utah", location: "Salt Lake City, Utah, United States", dedicated: "1893, April, 6", area: 382207, imageUrl: "https://churchofjesuschristtemples.org/salt-lake-temple/photographs/#Gallery-12" }
 ];
 
 // Function to create and display temple cards
